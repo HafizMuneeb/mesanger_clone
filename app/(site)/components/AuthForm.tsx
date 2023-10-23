0843300a67a1a1a44b0d4eb391ade15c0959c627
Muneeb <muneebafzal381a@gmail.com>
2023-10-23 19:29:47 +0500

    complete deployment of application

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,48 +1,56 @@
-"use client";
+'use client';
 
-import Button from "@/app/components/Button";
-import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import AuthSocialButton from "./AuthSocialButton";
-import {BsGithub, BsGoogle} from 'react-icons/bs';
 import axios from "axios";
-import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
 import { signIn, useSession } from 'next-auth/react';
+import { useCallback, useEffect, useState } from 'react';
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from "next/navigation";
 
+import Input from "@/app/components/inputs/Input";
+import AuthSocialButton from './AuthSocialButton';
+import Button from "@/app/components/Button";
+import { toast } from "react-hot-toast";
 
-
-type Variant = "LOGIN" | "REGISTER";
+type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>("LOGIN");
-  const [isLoading, setIsLoading] = useState(false);
+  const session = useSession();
   const router = useRouter();
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status === 'authenticated') {
+      router.push('/conversations')
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
-    if (variant === "LOGIN") {
-      setVariant("REGISTER");
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
     } else {
-      setVariant("LOGIN");
+      setVariant('LOGIN');
     }
   }, [variant]);
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: {
+      errors,
+    }
   } = useForm<FieldValues>({
     defaultValues: {
-      name: "",
-      email: "",
-      password: "",
-    },
+      name: '',
+      email: '',
+      password: ''
+    }
   });
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-
+  
     if (variant === 'REGISTER') {
       axios.post('/api/register', data)
       .then(() => signIn('credentials', {
@@ -73,17 +81,16 @@ const AuthForm = () => {
         }
 
         if (callback?.ok) {
-          toast.success('Logged In!')
           router.push('/conversations')
         }
       })
       .finally(() => setIsLoading(false))
     }
+  }
 
-  };
-
-  const socialActions = (action: string) => {
+  const socialAction = (action: string) => {
     setIsLoading(true);
+
     signIn(action, { redirect: false })
       .then((callback) => {
         if (callback?.error) {
@@ -95,75 +102,113 @@ const AuthForm = () => {
         }
       })
       .finally(() => setIsLoading(false));
-  };
-  return (
+  } 
+
+  return ( 
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          {variant === "REGISTER" && (
-            <Input id="name" label="Name" register={register} errors={errors} />
+      <div 
+        className="
+        bg-white
+          px-4
+          py-8
+          shadow
+          sm:rounded-lg
+          sm:px-10
+        "
+      >
+        <form 
+          className="space-y-6" 
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {variant === 'REGISTER' && (
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="name" 
+              label="Name"
+            />
           )}
-          <Input
-            id="email"
-            label="Email Address"
+          <Input 
+            disabled={isLoading}
             register={register}
+            errors={errors}
+            required
+            id="email" 
+            label="Email address" 
             type="email"
-            errors={errors}
-            disabled={isLoading}
           />
-          <Input
-            id="password"
-            label="Password"
-            register={register}
-            type="password"
-            errors={errors}
+          <Input 
             disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="password" 
+            label="Password" 
+            type="password"
           />
           <div>
             <Button disabled={isLoading} fullWidth type="submit">
-              {variant === "LOGIN" ? "Sign in" : "Register"}
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
             </Button>
           </div>
         </form>
 
         <div className="mt-6">
           <div className="relative">
-            <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-300" />
+            <div 
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-                <span className="bg-white px-2 text-gray-500">
-                    Or Continue with
-                </span>
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
             </div>
           </div>
-
 
           <div className="mt-6 flex gap-2">
             <AuthSocialButton 
-                icon={BsGithub}
-                onClick={() => socialActions('github')}
+              icon={BsGithub} 
+              onClick={() => socialAction('github')} 
             />
             <AuthSocialButton 
-                icon={BsGoogle}
-                onClick={() => socialActions('google')}
+              icon={BsGoogle} 
+              onClick={() => socialAction('google')} 
             />
           </div>
         </div>
-        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
-            <div>
-                {variant === 'LOGIN' ? 'New to Messanger' : 'Already have an account?'}
-            </div>
-            <div
-                onClick={toggleVariant}
-                className="underline cursor-pointer"
-            >
-                {variant === 'LOGIN' ? 'Create an accout' : 'Login'}
-            </div>
+        <div 
+          className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'} 
+          </div>
+          <div 
+            onClick={toggleVariant} 
+            className="underline cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
         </div>
       </div>
     </div>
   );
-};
-
+}
+ 
 export default AuthForm;
